9ebce94ae6f267ed295b9328323c5500878e7b64
Yashwantyadav1 <yy888275@gmail.com>
2023-10-19 22:55:55 +0530

    implements of switch case

---
+++ switch_case.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day=6;
+    switch (day)
+    {
+    case 6:
+        cout<<"today is saturday";
+
+        break;
+        case 7:
+        cout<<"today is sunday";
+        break;
+    
+    default:
+        cout<<"loking forward to the weekend";
+        break;
+    }
+    return 0;
+
+}
